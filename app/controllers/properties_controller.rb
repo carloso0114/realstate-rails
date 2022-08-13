255b5d714be393074b6b1007669de4ef6d0721f7
Carlos Ospina <carlossospinag@gmail.com>
2022-08-13 13:43:19 -0500

    create post request to api

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -24,4 +24,35 @@ class PropertiesController < ApplicationController
     client = EasyBroker.client.properties.find(@id)
     @property = client
   end
+
+  def contact_request
+      data = {
+        "name": params[:name],
+        "phone": params[:phone],
+        "email": params[:email],
+        "property_id": params[:property_id],
+        "message": params[:message],
+        "source": params[:source]
+      }.to_json
+      api_key = EASYBROKER
+      response = HTTParty.post('https://api.stagingeb.com/v1/contact_requests',
+        :headers => {'Content-Type' => 'application/json', 'X-Authorization' => api_key },
+        :body => data
+      )
+
+      if response.code === 422 
+        redirect_to request.referrer, alert: 'Debes especificar al menos un campo de nombre, email, numero de telefono.'
+      elsif response.code === 404
+        redirect_to request.referrer, alert: 'La propiedad especificada no pudo ser encontrada'
+      elsif response.code === 401
+        redirect_to request.referrer, alert: 'Api key invalida.'
+      elsif response.code === 200
+        redirect_to root_path, notice: 'Mensaje enviado exitosamente, seras contactado por uno de nuestros asesores en menos de 3 dias hábiles.'
+      else 
+        redirect_to request.referrer, alert: response.body + " " + response.code.to_s + " " + response.message
+      end
+
+      
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root :to => redirect('/properties/1')
   get "/properties/:id", to: "properties#index"
   get '/property/:id', to: 'properties#show'
+  post 'property/contact_request', to: "properties#contact_request"
 end
