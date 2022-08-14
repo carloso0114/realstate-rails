de7112d8ec171f8cfdcf20b4150e4e0a6f6313f4
Carlos Ospina <carlossospinag@gmail.com>
2022-08-14 05:28:41 -0500

    fix indentation on controllers

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,4 @@
 class PropertiesController < ApplicationController
-
   EasyBroker.configure do |config|
     config.api_key = 'l7u502p8v46ba3ppgvj5y2aad50lb9'
   
@@ -13,10 +12,10 @@ class PropertiesController < ApplicationController
   results = client.properties.search(search: { statuses: [:published] }, limit: 15, page: @id)
 
   @propertiesArray = []
+
   results.each do |property|
     @propertiesArray.push(property)
   end
-
   end
 
   def show
@@ -26,33 +25,29 @@ class PropertiesController < ApplicationController
   end
 
   def contact_request
-      data = {
-        "name": params[:name],
-        "phone": params[:phone],
-        "email": params[:email],
-        "property_id": params[:property_id],
-        "message": params[:message],
-        "source": params[:source]
-      }.to_json
-      api_key = EASYBROKER
-      response = HTTParty.post('https://api.stagingeb.com/v1/contact_requests',
-        :headers => {'Content-Type' => 'application/json', 'X-Authorization' => api_key },
-        :body => data
-      )
-
-      if response.code === 422 
-        redirect_to request.referrer, alert: 'Debes especificar al menos un campo de nombre, email, numero de telefono.'
-      elsif response.code === 404
-        redirect_to request.referrer, alert: 'La propiedad especificada no pudo ser encontrada'
-      elsif response.code === 401
-        redirect_to request.referrer, alert: 'Api key invalida.'
-      elsif response.code === 200
-        redirect_to root_path, notice: 'Mensaje enviado exitosamente, seras contactado por uno de nuestros asesores en menos de 3 dias hábiles.'
-      else 
-        redirect_to request.referrer, alert: response.body + " " + response.code.to_s + " " + response.message
-      end
-
-      
+    data = {
+      "name": params[:name],
+      "phone": params[:phone],
+      "email": params[:email],
+      "property_id": params[:property_id],
+      "message": params[:message],
+      "source": params[:source]
+    }.to_json
+    api_key = EASYBROKER
+    response = HTTParty.post('https://api.stagingeb.com/v1/contact_requests',
+      :headers => {'Content-Type' => 'application/json', 'X-Authorization' => api_key },
+      :body => data
+    )
+    if response.code === 422 
+      redirect_to request.referrer, alert: 'Debes especificar al menos un campo de nombre, email, numero de telefono.'
+    elsif response.code === 404
+      redirect_to request.referrer, alert: 'La propiedad especificada no pudo ser encontrada'
+    elsif response.code === 401
+      redirect_to request.referrer, alert: 'Api key invalida.'
+    elsif response.code === 200
+      redirect_to root_path, notice: 'Mensaje enviado exitosamente, seras contactado por uno de nuestros asesores en menos de 3 dias hábiles.'
+    else 
+      redirect_to request.referrer, alert: response.body + " " + response.code.to_s + " " + response.message
+    end
   end
-
 end
